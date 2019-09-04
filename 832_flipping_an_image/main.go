873bf0f1529adc0a6a7d1370c510acef18bff92e
crashiura <crashiura@gmail.com>
2019-09-04 18:07:01 +0300

    add bitwise operation

--- 832_flipping_an_image/main.go
+++ 832_flipping_an_image/main.go
@@ -7,26 +7,14 @@ func flipAndInvertImage(A [][]int) [][]int {
 	for _, v := range A {
 		for i, j := 0, len(v)-1; i < j; i, j = i+1, j-1 {
 			v[i], v[j] = v[j], v[i]
-
-			if v[j] == 0 {
-				v[j] = 1
-			} else {
-				v[j] = 0
-			}
-			if v[i] == 0 {
-				v[i] = 1
-			} else {
-				v[i] = 0
-			}
+			// ^ is the binary XOR operator. 0 ^ 1 = 1, 1 ^ 1 = 0
+			v[j] = v[j] ^ 1
+			v[i] = v[i] ^ 1
 		}
 
 		l := len(v)
 		if l%2 != 0 {
-			if v[l/2] == 0 {
-				v[l/2] = 1
-			} else {
-				v[l/2] = 0
-			}
+			v[l/2] = v[l/2] ^ 1
 		}
 	}
 
